83413c181c9f5e4ad97cb33daa5031ab97808887
vproskurin <vladimir.proskurin@gmail.com>
2020-04-01 19:45:37 -0700

    questions function and getting answers

---
+++ src/components/Header.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        <b-nav tabs>
+        <b-nav-item disabled><b>Fancy quiz app</b></b-nav-item>
+        <b-nav-item disabled>Count: 4/10</b-nav-item>
+        </b-nav>
+    </div>
+</template> 
